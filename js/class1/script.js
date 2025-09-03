0ed2eec77dcd3ec2f3bfbe192f4bff46aaa9034c
rajesh07S <rajeshsunar2021ad@gmail.com>
2025-09-03 18:10:37 +0545

    new something

---
+++ js/class1/script.js
@@ -0,0 +1,29 @@
+console.log('hello i am rajesh');
+function sum(x,y)
+{
+    console.log(x+y);
+}
+sum(4,7);   
+
+function product(a,b)
+{
+    return a*b;
+}
+let l=5;
+let i=6;
+product= product(l,i);
+console.log('product is:',product);
+
+function cal(p,r)
+{
+    let sum=p+r;
+    let diff=p-r;
+    let div=p/r;
+    let mul=p*r;
+    return sum;
+}
+let h=2;
+let k=3;
+calculation=cal(h,k)
+console.log('calculation',calculation);
+
